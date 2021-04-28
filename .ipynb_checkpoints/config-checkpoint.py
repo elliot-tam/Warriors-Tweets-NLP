d84f55e0611cb93144db29c962215a41fa800a3d
elliot-tam <elliot.tam8@gmail.com>
2021-04-27 17:48:00 -0700

    initial upload all files

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,5 @@
+	
+api_key = "cM7JxnQCwWnrN1EW4TPq6q5ZI"
+api_secret = "pSI95php3xiAIOS4uSfEkedtudHbxM9gXCUlJK6yPc8VtZJIBX"
+access_token = "86196433-yjKh2V3gQlaDqZyqQrys3vaJlQIKwfce0dH39rzRI"
+token_secret = "80abld786ZhLFFYleIbhLIh196xasScy9GgKBpcB3LMdh"
